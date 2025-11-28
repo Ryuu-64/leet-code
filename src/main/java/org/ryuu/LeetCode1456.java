89bf3c61583dc541e38871a53ca1ebac06859fe0
Ryuu <2357622935@qq.com>
2025-11-28 18:58:55 +0800

    feat

---
+++ src/main/java/org/ryuu/LeetCode1456.java
@@ -0,0 +1,13 @@
+package org.ryuu;
+
+/**
+ * <a href="https://leetcode.cn/problems/maximum-number-of-vowels-in-a-substring-of-given-length/description/">1456. 定长子串中元音的最大数目</a>
+ * TODO
+ */
+public class LeetCode1456 {
+    class Solution {
+        public int maxVowels(String s, int k) {
+
+        }
+    }
+}
