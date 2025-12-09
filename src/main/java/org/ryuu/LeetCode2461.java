b9b7f2ded5d9b772684a1b825ccda03f56f15ded
Ryuu <2357622935@qq.com>
2025-12-09 16:56:23 +0800

    feat: 滑动窗口

---
+++ src/main/java/org/ryuu/LeetCode2461.java
@@ -0,0 +1,41 @@
+package org.ryuu;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *<a href="https://leetcode.cn/problems/maximum-sum-of-distinct-subarrays-with-length-k/">2461. 长度为 K 子数组中的最大和</a>
+ */
+public class LeetCode2461 {
+    public static class Solution {
+        public long maximumSubarraySum(int[] nums, int k) {
+            long maxSum = 0;
+            long sum = 0;
+            Map<Integer, Integer> numCountMap = new HashMap<>();
+            for (int right = 0; right < nums.length; right++) {
+                int rightNum = nums[right];
+                sum += rightNum;
+                numCountMap.merge(rightNum, 1, Integer::sum);
+                int left = right - k + 1;
+                if (left < 0) {
+                    continue;
+                }
+
+                if (numCountMap.size() == k) {
+                    maxSum = Math.max(sum, maxSum);
+                }
+
+                int leftNum = nums[left];
+                sum -= leftNum;
+                int count = numCountMap.getOrDefault(leftNum, 0);
+                if (count > 1) {
+                    numCountMap.put(leftNum, count - 1);
+                } else {
+                    numCountMap.remove(leftNum);
+                }
+            }
+
+            return maxSum;
+        }
+    }
+}
---
+++ src/test/java/org/ryuu/LeetCode2461Test.java
@@ -0,0 +1,15 @@
+package org.ryuu;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class LeetCode2461Test {
+    @Test
+    public void maximumSubarraySum() {
+        LeetCode2461.Solution solution = new LeetCode2461.Solution();
+        int[] nums = {1, 5, 4, 2, 9, 9, 9};
+        int k = 3;
+        long sum = solution.maximumSubarraySum(nums, k);
+        Assertions.assertEquals(15, sum);
+    }
+}
