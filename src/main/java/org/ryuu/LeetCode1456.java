7c83ba39fec8eb217ac8c30ef104086b319e43ce
Ryuu <2357622935@qq.com>
2025-12-15 15:50:38 +0800

    feat: 1456. 定长子串中元音的最大数目（定长滑动窗口）

--- src/main/java/org/ryuu/LeetCode1456.java
+++ src/main/java/org/ryuu/LeetCode1456.java
@@ -1,13 +1,46 @@
 package org.ryuu;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * <a href="https://leetcode.cn/problems/maximum-number-of-vowels-in-a-substring-of-given-length/description/">1456. 定长子串中元音的最大数目</a>
- * TODO
  */
 public class LeetCode1456 {
-    class Solution {
+    public static class Solution {
+        private static final Set<Character> ALL_VOWELS = new HashSet<>();
+
+        static {
+            ALL_VOWELS.add('a');
+            ALL_VOWELS.add('e');
+            ALL_VOWELS.add('i');
+            ALL_VOWELS.add('o');
+            ALL_VOWELS.add('u');
+        }
+
         public int maxVowels(String s, int k) {
-            return 0;
+            int maxVowels = 0;
+            int vowels = 0;
+            for (int right = 0; right < s.length(); right++) {
+                char rightChar = s.charAt(right);
+                if (ALL_VOWELS.contains(rightChar)) {
+                    vowels++;
+                }
+
+                if (right < k - 1) {
+                    continue;
+                }
+
+                maxVowels = Math.max(vowels, maxVowels);
+
+                int left = right - k + 1;
+                char leftChar = s.charAt(left);
+                if (ALL_VOWELS.contains(leftChar)) {
+                    vowels--;
+                }
+
+            }
+            return maxVowels;
         }
     }
 }
